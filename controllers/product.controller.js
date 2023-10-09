4720ddab33e446cb1052667b59d5c8785d5cbbe6
Andrea Olivos <andrea.olivos.b@gmail.com>
2023-10-09 17:54:49 -0300

    Add getProductByName controller

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -7,10 +7,24 @@ export const getAllProductsFromCategory = async (req, res) => {
 
   try {
     const allProductsFromCategory = await Product.find({ category: category });
-
     res.status(200).json(allProductsFromCategory);
 
   } catch (error) {
     res.status(404).json({ message: `No se pudo encontrar productos de la categoría ${category}: ${error}`});
+  }
+}
+
+
+// Get the product with the requested name
+export const getProductByName = async (req, res) => {
+
+  const { name } = req.params;
+
+  try {
+    const product = await Product.findOne({ name: name });
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(404).json({ message: `No se pudo encontrar el producto ${name}: ${error}`});
   }
 }
--- routes/product.router.js
+++ routes/product.router.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getAllProductsFromCategory } from '../controllers/product.controller.js';
+import { getAllProductsFromCategory, getProductByName } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.get('/products/:category', getAllProductsFromCategory);
+
+router.get('/product/:name', getProductByName);
 
 export default router;
